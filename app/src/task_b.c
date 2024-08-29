f6687eabc814e9481ce62cb8dc0fe26bab8fd5da
Denise Viltes <dviltes@fi.uba.ar>
2024-08-28 22:00:26 -0300

    añade código bloqueante y no bloqueante

--- app/src/task_b.c
+++ app/src/task_b.c
@@ -157,6 +157,38 @@ void task_b_update(void *parameters)
 	#if (TEST_X == TEST_2)
 
 	/* Here Chatbot Artificial Intelligence generated code */
+#include "stm32f1xx.h"
+
+volatile uint32_t msTicks = 0;
+
+void SysTick_Handler(void) {
+    msTicks++;
+}
+
+void delay_ms_non_blocking(uint32_t delay) {
+    uint32_t currentTicks = msTicks;
+    while ((msTicks - currentTicks) < delay) {
+        // Aquí el código es no bloqueante, podrías ejecutar otras tareas
+    }
+}
+
+int main(void) {
+    HAL_Init();
+    SysTick_Config(SystemCoreClock / 1000);  // Configura SysTick para que interrumpa cada 1ms
+
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_13;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    while (1) {
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        delay_ms_non_blocking(1000);  // Espera no bloqueante de 1 segundo
+    }
+}
+
 
 	#endif
 }
